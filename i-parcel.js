cbb41ad8c5c9f532b26e651784e755ec08bb1b03
Marco Asbreuk <marco@itsasbreuk.nl>
2015-03-02 00:17:03 +0100

    intermediate saving

--- i-parcel.js
+++ i-parcel.js
@@ -8,7 +8,21 @@ module.exports = function (window) {
         Itag;
 
     if (!window.ITAGS[itagName]) {
-        Itag = DOCUMENT.createItag(itagName, null, false); // not subclassable, only pseudoclassable
+        Itag = DOCUMENT.createItag(itagName, {
+            init: {
+                var element = this,
+                    designNode = element.getItagContainer(),
+                    content = designNode.getHTML();
+                // when initializing: make sure NOT to overrule model-properties that already
+                // might have been defined when modeldata was boundend. Therefore, use `defineWhenUndefined`
+                // element.defineWhenUndefined('someprop', somevalue); // sets element.model.someprop = somevalue; when not defined yet
+                element.defineWhenUndefined('content', content);
+            },
+            sync: function() {
+                var element = this;
+                element.setHTML(element.model.content);
+            }
+        });
         itagCore.setContentVisibility(Itag, true);
         window.ITAGS[itagName] = Itag;
     }
