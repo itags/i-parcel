57e67cb096110b382c409c2512a269b35332acf4
Marco Asbreuk <marco@itsasbreuk.nl>
2015-03-19 01:03:21 +0100

    intermediate saving

--- i-parcel.js
+++ i-parcel.js
@@ -3,6 +3,7 @@ module.exports = function (window) {
 
     var itagCore = require('itags.core')(window),
         itagName = 'i-parcel', // <-- define your own itag-name here
+        microtemplate = require('micro-template').template,
         DOCUMENT = window.document,
         Itag;
 
@@ -11,15 +12,23 @@ module.exports = function (window) {
             init: function() {
                 var element = this,
                     designNode = element.getItagContainer(),
-                    content = designNode.getHTML();
+                    template = designNode.getHTML();
                 // when initializing: make sure NOT to overrule model-properties that already
                 // might have been defined when modeldata was boundend. Therefore, use `defineWhenUndefined`
                 // element.defineWhenUndefined('someprop', somevalue); // sets element.model.someprop = somevalue; when not defined yet
-                element.defineWhenUndefined('content', content);
+                //
+                // we MUST avoid circular reference: we cannot accept `template` as a property-field --> we remove those.
+                // Also, because < and > are escaped by text-nodes: we need to unescape them:
+                template = template.replaceAll('template', '')
+                                   .replaceAll('&lt;', '<')
+                                   .replaceAll('&gt;', '>');
+console.warn(template);
+                element.defineWhenUndefined('template', template);
             },
             sync: function() {
-                var element = this;
-                element.setHTML(element.model.content);
+                var element = this,
+                    model = element.model;
+                element.setHTML(microtemplate(model.template, model));
             }
         });
         itagCore.setContentVisibility(Itag, true);
